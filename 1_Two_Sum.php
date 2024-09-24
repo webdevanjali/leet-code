c3df51a1a17fc843e2d2a1a06c49213adf196b10
fun404 <anjalpandey73@gmail.com>
2024-09-24 23:59:39 +0530

    1. Two Sum [Difficulty level:Easy] Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order. Example 1:Input: nums = [2,7,11,15], target = 9 Output: [0,1] Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].Example 2:Input: nums = [3,2,4], target = 6 Output: [1,2]Example 3:Input: nums = [3,3], target = 6 Output: [0,1] Constraints:    2 <= nums.length <= 104     -109 <= nums[i] <= 109     -109 <= target <= 109     Only one valid answer exists. Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?

---
+++ 1_Two_Sum.php
@@ -0,0 +1,23 @@
+// your code goes here
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+     function twoSum($nums, $target) {
+        $index=[];
+        foreach($nums as $i => $num){
+            $complement = $target - $num;
+            if ($complement==$num && count(array_keys($nums, $complement))>1){
+               $index= array_keys($nums, $complement);
+                return $index;
+            }
+            elseif ($complement!=$num && array_search($complement, $nums)){
+               array_push($index, $i,array_search($complement, $nums));
+                return $index;
+            }
+        }
+    }
+}
---
+++ 386_Lexicographical_Numbers.php
@@ -0,0 +1,27 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer[]
+     */
+    function lexicalOrder($n) {
+        $lexi=array();
+        $leadingDigit=1;
+        for($i=1;$i<=$n;$i++){
+            array_push($lexi,$leadingDigit);
+            if($leadingDigit*10<=$n){
+                $leadingDigit*=10;
+            }else{
+                while($leadingDigit%10==9 || $leadingDigit>=$n){
+                    $leadingDigit= floor($leadingDigit/10);
+                    
+                }
+                $leadingDigit++;
+            }
+        }
+        return $lexi;
+    }
+}
+
+?>

01cb2939b6a149bfcb6a5bf288677fb6d550bb16
fun404 <anjalpandey73@gmail.com>
2024-10-09 23:57:40 +0530

    [Problem Difficulty : Easy] 2769_Find_the_Maximum_Achievable_Number
    Given two integers, num and t. A number is achievable if it can become equal to num after applying the following operation:
        Increase or decrease the number by 1, and simultaneously increase or decrease num by 1.
    Return the maximum achievable number after applying the operation at most t times.
    Example 1:
    	Input: num = 4, t = 1
    	Output: 6
    	Explanation:
    	Apply the following operation once to make the maximum achievable number equal to num:
    	    Decrease the maximum achievable number by 1, and increase num by 1.
    Example 2:
    	Input: num = 3, t = 2
    	Output: 7
    	Explanation:
    	Apply the following operation twice to make the maximum achievable number equal to num:
    	    Decrease the maximum achievable number by 1, and increase num by 1.
    Constraints:
        1 <= num, t <= 50

---
+++ 2769_Find_the_Maximum_Achievable_Number.php
@@ -0,0 +1,12 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @param Integer $t
+     * @return Integer
+     */
+    function theMaximumAchievableX($num, $t) {
+        return $num+2*$t;
+    }
+}

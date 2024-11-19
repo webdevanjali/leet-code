28f04b0c7be257bf8ff6a4a77429188108b1690b
fun404 <anjalpandey73@gmail.com>
2024-11-19 23:59:33 +0530

    ss

---
+++ 2089_Find_Target_Indices_After_Sorting_Array.php
@@ -0,0 +1,18 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function targetIndices($nums, $target) {
+        sort($nums);
+        $list=[];
+        for($i=0; $i<count($nums); $i++){
+            if($nums[$i]==$target) $list[] = $i;
+            elseif($nums[$i]>$target) return $list;
+        }
+        return $list;
+    }
+}
